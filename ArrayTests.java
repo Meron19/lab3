25e3732a4e4fc89cf31e3bb6d64e7881bf0aeb6c
Meron19 <meronamershaoo@gmail.com>
2023-04-20 14:52:26 -0700

    Fixed bugs for reverseInPlace and reversed

--- ArrayTests.java
+++ ArrayTests.java
@@ -8,11 +8,22 @@ public class ArrayTests {
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
-
-
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test 
+	public void testReverseInPlace2() {
+    int[] input1 = {3, 1, 5};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{5, 1, 3}, input1);
+	}
+
+  @Test
+  public void testReversed2() {
+    int[] input1 = {1, 2, 3};
+    assertArrayEquals(new int[] {3, 2, 1}, ArrayExamples.reversed(input1));
   }
 }
